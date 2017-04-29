1363451d7362fbf340ef57fd5c6053ed91481a15
Sergey Matveev <stargrave@stargrave.org>
2017-04-29 23:21:08 +0300

    Russian news page

--- news_and_install.sh
+++ news_and_install.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
 texi=`mktemp`
+
 cat > $texi <<EOF
 \input texinfo
 @documentencoding UTF-8
 @settitle NEWS
 
-`cat doc/news.texi`
+`sed -n '5,$p' < doc/news.texi`
 
 @bye
 EOF
 makeinfo --plaintext -o NEWS $texi
+
+cat > $texi <<EOF
+\input texinfo
+@documentencoding UTF-8
+@settitle NEWS.RU
+
+@node Новости
+@unnumbered Новости
+
+`sed -n '3,$p' < doc/news.ru.texi | sed 's/^@subsection/@section/'`
+
+@bye
+EOF
+makeinfo --plaintext -o NEWS.RU $texi
+
 rm -f $texi
 
 texi=$(TMPDIR=doc mktemp)
