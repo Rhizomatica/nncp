d2cdb26385f60097a31af54f0e5f3447b1289bd1
Sergey Matveev <stargrave@stargrave.org>
2018-09-08 17:30:36 +0300

    Leading dots in packages description

--- src/cypherpunks.ru/nncp/cmd/nncp-stat/main.go
+++ src/cypherpunks.ru/nncp/cmd/nncp-stat/main.go
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// Show queued NNCP Rx/Tx stats
+// Show queued NNCP Rx/Tx stats.
 package main
 
 import (
