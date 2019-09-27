dd92823db3d72fb21a4c712a7fb052dce16443dd
Sergey Matveev <stargrave@stargrave.org>
2019-09-27 13:03:31 +0300

    Forbid any later GNU GPL versions autousage

--- src/cypherpunks.ru/nncp/tmp.go
+++ src/cypherpunks.ru/nncp/tmp.go
@@ -4,8 +4,7 @@ Copyright (C) 2016-2019 Sergey Matveev <stargrave@stargrave.org>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+the Free Software Foundation, version 3 of the License.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
