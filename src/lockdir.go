49eb94bbf7a0c157764e423f6b5e4a21143e9dd8
Sergey Matveev <stargrave@stargrave.org>
2021-03-11 12:05:28 +0300

    Remove excess %s in log messages

--- src/lockdir.go
+++ src/lockdir.go
@@ -36,14 +36,14 @@ func (ctx *Ctx) LockDir(nodeId *NodeId, lockCtx string) (*os.File, error) {
 	)
 	if err != nil {
 		ctx.LogE("lockdir-open", LEs{{"Path", lockPath}}, err, func(les LEs) string {
-			return "Locking directory: opening %s" + lockPath
+			return "Locking directory: opening " + lockPath
 		})
 		return nil, err
 	}
 	err = unix.Flock(int(dirLock.Fd()), unix.LOCK_EX|unix.LOCK_NB)
 	if err != nil {
 		ctx.LogE("lockdir-flock", LEs{{"Path", lockPath}}, err, func(les LEs) string {
-			return "Locking directory: locking %s" + lockPath
+			return "Locking directory: locking " + lockPath
 		})
 		dirLock.Close() // #nosec G104
 		return nil, err
