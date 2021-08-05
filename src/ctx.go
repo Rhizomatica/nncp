5b4b68b15d90d00ad591f8c2ae54bb1809834478
Sergey Matveev <stargrave@stargrave.org>
2021-08-05 15:42:05 +0300

    NetBSD compatible IsEnoughSpace()

--- src/ctx.go
+++ src/ctx.go
@@ -21,13 +21,10 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
 
 	"syscall"
-
-	"golang.org/x/sys/unix"
 )
 
 type Ctx struct {
@@ -147,14 +144,6 @@ func CtxFromCmdline(
 	ctx.Quiet = quiet
 	ctx.Debug = debug
 	return ctx, nil
-}
-
-func (ctx *Ctx) IsEnoughSpace(want int64) bool {
-	var s unix.Statfs_t
-	if err := unix.Statfs(ctx.Spool, &s); err != nil {
-		log.Fatalln("Can not stat spool:", err)
-	}
-	return int64(s.Bavail)*int64(s.Bsize) > want
 }
 
 func (ctx *Ctx) Umask() {
