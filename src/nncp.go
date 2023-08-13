2c8c42466ffd977652d3468681e2c69bdc8fae6d
Sergey Matveev <stargrave@stargrave.org>
2023-08-13 19:49:16 +0300

    Do not use multiline const-s with single entry

--- src/nncp.go
+++ src/nncp.go
@@ -37,9 +37,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.`
+	Base32Encoded32Len = 52
 )
-
-const Base32Encoded32Len = 52
 
 var (
 	Version string = "8.8.4"
