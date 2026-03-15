2664f33cd098e9b7705cc4335bb63e91503ec3b7
Sergey Matveev <stargrave@stargrave.org>
2026-03-15 10:56:15 +0300

    Remove from copyright authors
    As currently I did not touched those files at all.

--- src/hfmodem/listener.go
+++ src/hfmodem/listener.go
@@ -1,5 +1,4 @@
 // NNCP -- Node to Node copy, utilities for store-and-forward data exchange
-// Copyright (C) 2016-2026 Sergey Matveev <stargrave@stargrave.org>
 // Copyright (C) 2026 Rhizomatica <rafael@rhizomatica.org>
 //
 // This program is free software: you can redistribute it and/or modify
